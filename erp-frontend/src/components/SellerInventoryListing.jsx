1e6a06e80c751d860d38093f0366fc78fb244b77
Akash Chourasia <akash.chourasia@luminad.com>
2024-12-21 22:53:13 +0530

    Changes in the frontend of Seller Inventory

--- erp-frontend/src/components/SellerInventoryListing.jsx
+++ erp-frontend/src/components/SellerInventoryListing.jsx
@@ -1,9 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { Container,Table } from 'react-bootstrap';
 
 export default function SellerInventoryListing(){
+    const [loading,setLoading] = useState(false)
     return(
         <>
-        <div>Hi</div>
+        <Container className="mt-4">
+        <h1 className="text-center mb-4">Data Table</h1>
+        {loading? (<p>Loading...</p>):(
+            <Table striped bordered hover responsive="md" className="custom-table">
+                <thead className="table-dark">
+                    <tr>
+                        <th>Product</th>
+                        <th>Brand</th>
+                        <th>Model</th>
+                        <th>Color</th>
+                        <th>Hsn Code</th>
+                        <th>Quantity</th>
+                        <th>Unit Price</th>
+                        <th>Gross Price</th>
+                        <th>Discount %</th>
+                        <th>Net Price</th>
+                        <th>GST %</th>
+                        <th>Final Amount</th>
+                        <th>Created On</th>
+                        <th>Updated On</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    
+                </tbody>
+            </Table>
+        )}
+        </Container>
+
         </>
     )
 }
